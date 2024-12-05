b14f5a8952aba8998790780cce8a7c117e740713
Apeksha Tembhurne <appy17@users.noreply.github.com>
2024-12-05 17:37:44 +0530

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
